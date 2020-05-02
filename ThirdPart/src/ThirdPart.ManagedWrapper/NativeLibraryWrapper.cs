2a4ccb4922e1647d97fa21c396960d23bf7c003a
TeBeCo <tebeco@gmail.com>
2020-05-02 16:43:40 +0200

    Use "NativeLibrary" to load thirdpart

--- ThirdPart/src/ThirdPart.ManagedWrapper/NativeLibraryWrapper.cs
+++ ThirdPart/src/ThirdPart.ManagedWrapper/NativeLibraryWrapper.cs
@@ -1,10 +1,23 @@
+using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace ThirdPart.ManagedWrapper
 {
+
     public static class NativeLibraryWrapper
     {
-        [DllImport("third_part_native.dll")]
+#if NETCOREAPP3_0 || NETCOREAPP3_1 || NETCOREAPP5_0
+        static NativeLibraryWrapper()
+        {
+            NativeLibrary.SetDllImportResolver(typeof(NativeLibraryWrapper).Assembly, MapAndLoad);
+
+            static IntPtr MapAndLoad(string libraryName, Assembly assembly, DllImportSearchPath? dllImportSearchPath) =>
+                NativeLibrary.Load(libraryName, assembly, dllImportSearchPath);
+        }
+
+#endif
+        [DllImport("third_part_native")]
         public static extern int do_stuff(int num);
     }
 }
