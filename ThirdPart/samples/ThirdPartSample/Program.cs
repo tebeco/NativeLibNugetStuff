5068e5bceb7a20005399aa057765643667c99ce8
TeBeCo <tebeco@gmail.com>
2020-05-02 11:23:34 +0200

    Adding sample in ThirdPart

---
+++ ThirdPart/samples/ThirdPartSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ThirdPart.ManagedWrapper;
+
+namespace ThirdPartSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = NativeLibrary.do_stuff(4);
+            Console.WriteLine($"The third part lib returned : {result}");
+        }
+    }
+}
+
+
